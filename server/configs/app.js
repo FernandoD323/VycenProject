5ad959ed2d6421906b9ff00f1d18fea3e6cf286e
fdiaz-2017264 <fdiaz-2017264@kinal.edu.gt>
2023-11-26 18:42:51 -0600

    Función para guardar las imágenes

--- server/configs/app.js
+++ server/configs/app.js
@@ -1,8 +1,10 @@
 'use strict'
 
 const express = require('express');
+const fileUpload = require('express-fileupload');
 const morgan = require('morgan');
 const cors = require('cors');
+const path = require('path')
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const userRoutes = require('../src/routes/user.routes');
@@ -29,6 +31,40 @@ app.use('/forum', forumRoutes);
 app.use('/gallery', galleryRoutes);
 app.use('/reports', reportRoutes);
 
+app.use(fileUpload());
+
+// Ruta para subir archivos
+app.post('/upload', (req, res) => {
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).json({ message: 'No files were uploaded.' });
+  }
+
+  const sampleFile = req.files.sampleFile;
+
+  // Lista de extensiones permitidas (puedes ajustarla según tus necesidades)
+  const allowedExtensions = ['.jpg', '.jpeg', '.png', '.gif'];
+
+  const fileExtension = path.extname(sampleFile.name).toLowerCase();
+
+  if (!allowedExtensions.includes(fileExtension)) {
+    return res.status(400).json({
+      message: 'Invalid file extension. Allowed extensions: ' + allowedExtensions.join(', ')
+    });
+  }
+
+  // Generar un nombre único para el archivo basado en la fecha actual
+  const uniqueFileName = `archivo_${Date.now()}${fileExtension}`;
+  const uploadPath = path.join(__dirname, '..','uploads', uniqueFileName);
+
+  // Mover el archivo al directorio de destino
+  sampleFile.mv(uploadPath, (err) => {
+    if (err) {
+      return res.status(500).json({ message: err.message });
+    }
+
+    res.json({ message: 'File uploaded!', fileName: uniqueFileName });
+  });
+});
 
 
 module.exports = app;
